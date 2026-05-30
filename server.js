f6a9aea51faa51d7c055cd068bd8ff4d3971f553
pjankushkumar-cmd <pj.ankushkumar@gmail.com>
2026-05-31 03:35:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-app.use(express.static('public'));
+// Sab files ko public folder se load karne ke liye
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Root URL par index.html dikhane ke liye
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Admin panel direct link
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
 
 io.on('connection', (socket) => {
-    // Admin jab update bhejega
     socket.on('adminUpdate', (data) => {
         io.emit('predictionUpdate', data);
     });
 });
 
-http.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
